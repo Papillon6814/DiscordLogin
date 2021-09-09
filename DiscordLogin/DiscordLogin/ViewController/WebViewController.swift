d70b69b3b2a53b4ad9a70f45b607f0847106de0f
Papillon6814 <so1rou.k@gmail.com>
2021-09-09 17:43:51 +0900

    feat: open discord and signin

---
+++ DiscordLogin/DiscordLogin/ViewController/WebViewController.swift
@@ -0,0 +1,44 @@
+//
+//  WebViewController.swift
+//  DiscordLogin
+//
+//  Created by Papillon on 2021/09/09.
+//
+
+import Foundation
+import WebKit
+
+class WebViewController: UIViewController, WKUIDelegate {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func loadView() {
+        super.loadView()
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let url = URL(string: "https://discord.com/api/oauth2/authorize?client_id=885399115763183656&redirect_uri=https%3A%2F%2Fe-players-web.web.app%2F&response_type=code&scope=email%20identify") else {
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
+
+extension WebViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        // ここでcode取得できそう
+        print(navigationAction.request.url)
+        
+        decisionHandler(.allow)
+        
+        
+        //dismiss(animated: true)
+    }
+}
